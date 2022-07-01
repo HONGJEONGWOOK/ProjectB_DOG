fb63e01391a32a8e29a1acc72ab6988062cb5459
SANG-SANG <66198932+pktony@users.noreply.github.com>
2022-07-01 12:27:37 +0900

    Goblin
    Behavior Tree 작업 중.
     - 작업 시간이 오래 걸릴 것으로 예상되어 일단 보류
    
    근접 몬스터 (고블린) 완료
     - Idle 상태
     - 탐지 범위 안에 있으면 쫓아가서, 공격 범위 안에서 공격 실행
       * 탐지범위 밖으로 벗어나면 다시 Idle상태

--- DOG/Assets/Scripts/Monster/Monsters.cs
+++ DOG/Assets/Scripts/Monster/Monsters.cs
@@ -9,20 +9,24 @@ public class Monsters : MonoBehaviour
     private CircleCollider2D circleColl = null;
 
     public Transform player = null;
-    public enum CurrentState { idle, traverse, track, attack, dead };
+    public enum CurrentState { idle, patrol, track, attack, dead };
     
     public static bool isDead = false;
+    private bool isPatrol = false;
 
     [Header("몬스터 기본스탯")]
     public float healthPoint = 100.0f;
     public int strength = 5;
     public float moveSpeed = 3.0f;
     public float attackRange = 1.5f;
+    public float attackSpeed = 2.0f;
 
     [Header("몬스터 AI 관련")]
     public CurrentState status = CurrentState.idle;
     private Vector2 trackDirection = Vector2.zero;
     public float detectRadius = 5.0f;
+
+    public Transform[] waypoint = null;
 
     public float HealthPoint { get => healthPoint; set { healthPoint = value; } }
 
@@ -30,8 +34,8 @@ public class Monsters : MonoBehaviour
     {
         rigid = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-
         circleColl = GetComponent<CircleCollider2D>();
+
         circleColl.radius = detectRadius;
     }
 
@@ -43,6 +47,10 @@ public class Monsters : MonoBehaviour
     private void FixedUpdate()
     {
         Move_Monster();
+        //if (isPatrol)
+        //{
+        //    Patrol();
+        //}
     }
 
     void Move_Monster()
@@ -56,26 +64,30 @@ public class Monsters : MonoBehaviour
         {
             if (status == CurrentState.idle)
             {
-                //idle 애니메이션 재생
+                anim.SetBool("isMoving", false);
                 trackDirection = Vector2.zero;
             }
-            else if (status == CurrentState.traverse)
+            else if (status == CurrentState.patrol)
             {
-
+                isPatrol = true;
             }
             else if (status == CurrentState.track)
             {
-                //Move 애니메이션 재생
+                anim.SetBool("isMoving", true);
                 trackDirection = player.position - this.transform.position;
                 if(trackDirection.sqrMagnitude < attackRange * attackRange)
                 {
                     status = CurrentState.attack;
+                    trackDirection = Vector2.zero;
                 }
             }
             else if (status == CurrentState.attack)
             {
+                anim.SetBool("isMoving", false);
+
+                transform.rotation = Quaternion.Euler( transform.position - player.position);
                 anim.SetTrigger("OnAttack");
-                status = CurrentState.track;
+                yield return new WaitForSeconds(attackSpeed);
             }
             else if (status == CurrentState.dead)
             {
@@ -85,6 +97,37 @@ public class Monsters : MonoBehaviour
             yield return null;
         }
     }
+
+    void Patrol()
+    {
+        bool isWait = true;
+        float waitCounter = 0.0f;
+        float waitTime = 2.0f;
+
+        waitCounter += Time.fixedDeltaTime;
+
+        if (waitCounter > waitTime)
+        {
+            isWait = false;
+        }
+
+        int waypointIndex = 0;
+        if (Vector2.SqrMagnitude(transform.position - waypoint[waypointIndex].position) < 0.01f)
+        {
+            //waypoint 변경
+            waitCounter = 0.0f;
+            isWait = true;
+
+            waypointIndex = (waypointIndex + 1) % waypoint.Length;
+        }
+        else
+        {
+            transform.position = Vector2.MoveTowards(transform.position, waypoint[waypointIndex].position,
+                Time.fixedDeltaTime * moveSpeed);
+            transform.LookAt(waypoint[waypointIndex]);
+        }
+    }
+
 
     private void OnTriggerEnter2D(Collider2D detection)
     {
@@ -97,8 +140,7 @@ public class Monsters : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        Gizmos.DrawWireSphere(transform.position, detectRadius);
+        Gizmos.DrawWireSphere(transform.position, attackRange * attackRange);
         //Gizmos.color = new Color(0.5f, 0.5f, 0.5f, 0.3f);
     }
-
 }
